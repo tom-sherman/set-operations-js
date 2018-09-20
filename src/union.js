30d9c1414136c4a4bf6b015528d816fb2cb9afc4
Tom Sherman <the.tomsherman@gmail.com>
2018-09-20 14:58:52 +0100

    Update todo

--- src/union.js
+++ src/union.js
@@ -5,6 +5,6 @@ import { flatten } from './utils'
  * @param  {...Set} sets
  */
 export function union (...sets) {
-  // TODO: Test performance vs nested for;of loop
+  // TODO: Test performance vs nested for;of loop vs reduce
   return new Set(flatten(sets.map(set => [...set])))
 }
