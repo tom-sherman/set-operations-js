1e1bc8cb6e0ac1c9cd94c175997ac581074f2107
Tom Sherman <the.tomsherman@gmail.com>
2018-09-20 13:56:04 +0100

    Add difference examples

--- test/difference.test.js
+++ test/difference.test.js
@@ -1,11 +1,11 @@
 const t = require('tap')
 const { difference, isEqual } = require('../index.js')
 
-
 t.test('difference', t => {
   const differences = [
     [ [1,2], [2,3], [1] ],
-    [ [null,1], [null,1,56], [] ]
+    [ [null,1], [null,1,56], [] ],
+    [ [1,2,3], [4,5,6], [1,2,3] ]
   ]
   differences.forEach(([a, b, diff]) => {
     const diffed = difference(new Set(a), new Set(b))
