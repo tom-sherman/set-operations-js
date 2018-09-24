5a0325f70b669efb12efe2690e2a6a2a63ff3d53
Tom Sherman <the.tomsherman@gmail.com>
2018-09-24 15:47:41 +0100

    Fix intersection tests

--- test/intersection.test.js
+++ test/intersection.test.js
@@ -33,9 +33,16 @@ t.test('intersection (2 or more sets)', t => {
 
   t.ok(isEqual(
     // The intersection of
-    intersection(new Set([ 1, 2, 3 ]), new Set([ 3, 4, 5 ]), new Set([ 4, 5, 6 ])),
+    intersection(new Set([ 1, 2, 3 ]), new Set([ 3, 4, 5 ]), new Set([ 3, 5, 6 ])),
     // Should equal
-    new Set([ 3, 4, 5 ])
+    new Set([ 3 ])
+  ))
+
+  t.ok(isEqual(
+    // The intersection of
+    intersection(new Set([ 1, 2, 3 ]), new Set([ 1, 2, 4 ]), new Set([ 1, 2 ]), new Set([ 1, 2 ])),
+    // Should equal
+    new Set([ 1, 2 ])
   ))
   t.end()
 })
